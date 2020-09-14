c3d96567e1da7fa8c94d01a5fdde4970134bd29c
mduczmal <miko.duczmal@gmail.com>
2020-09-14 21:23:24 +0200

    Init commit

---
+++ Beeper/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Media;
+using System.Threading;
+
+namespace Beeper
+{
+    class Player
+    {
+        public static void Init()
+        {
+            SoundPlayer beep06 = new SoundPlayer(Properties.Resources.beep_06);
+            SoundPlayer beep07 = new SoundPlayer(Properties.Resources.beep_07);
+            SoundPlayer beep08 = new SoundPlayer(Properties.Resources.beep_08);
+            SoundPlayer beep09 = new SoundPlayer(Properties.Resources.beep_09);
+            while (true)
+                try
+                {
+                    Console.WriteLine("You have 2 minutes");
+                    Thread.Sleep(5000);
+                    beep06.Play();
+                    Console.WriteLine("1 minute 10 seconds left");
+                    Thread.Sleep(5000);
+                    beep07.Play();
+                    Console.WriteLine("1 minute left");
+                    Thread.Sleep(5000);
+                    beep08.Play();
+                    Console.WriteLine("10 seconds left");
+                    Thread.Sleep(5000);
+                    beep09.Play();
+                }
+                catch (ThreadInterruptedException e)
+                {
+                    Console.WriteLine("Timer restarted");
+                }
+        }
+    }
+}
---
+++ Beeper/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Beeper
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+
+        private delegate bool EventHandler(CtrlType sig);
+        static EventHandler _handler;
+
+        private static Thread keyInterceptor;
+        private static Thread beepPlayer;
+
+        enum CtrlType
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+
+        private static bool Handler(CtrlType sig)
+        {
+            switch (sig)
+            {
+                case CtrlType.CTRL_C_EVENT:
+                    beepPlayer.Abort();
+                    keyInterceptor.Abort();
+                    return true;
+                case CtrlType.CTRL_LOGOFF_EVENT:
+                    beepPlayer.Abort();
+                    keyInterceptor.Abort();
+                    return true;
+                case CtrlType.CTRL_SHUTDOWN_EVENT:
+                    beepPlayer.Abort();
+                    keyInterceptor.Abort();
+                    return true;
+                case CtrlType.CTRL_CLOSE_EVENT:
+                    beepPlayer.Abort();
+                    keyInterceptor.Abort();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            _handler += new EventHandler(Handler);
+            SetConsoleCtrlHandler(_handler, true);
+            InterceptKeys interceptKeys = new InterceptKeys();
+            Player player = new Player();
+            beepPlayer = new Thread(new ThreadStart(Player.Init));
+            keyInterceptor = new Thread(InterceptKeys.Init);
+            beepPlayer.Start();
+            keyInterceptor.Start(beepPlayer);
+        }
+    }
+}
